345bed9f416ba761a44bf3de9f62026dfa1f28e1
Jose Moreno <josemorenodev@outlook.com>
2023-06-14 16:37:35 -0400

    Selecting and manipulating elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number! 🥳';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value =23;
+console.log(document.querySelector('.guess').value);
+
+*/
+
