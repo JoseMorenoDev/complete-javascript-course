f66334c5c0834e167fb0b0a3a45f886cc82753e8
Jose Moreno <josemorenodev@outlook.com>
2023-06-15 11:01:55 -0400

    first part completed!

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,4 +11,54 @@ document.querySelector('.guess').value =23;
 console.log(document.querySelector('.guess').value);
 
 */
+const secretNumber = Math.trunc(Math.random()*20) +1;
+let score =20;
 
+
+document.querySelector('.check').addEventListener('click', function (){
+    const guess= Number (document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    //When there is no input
+    if(!guess) {
+        document.querySelector('.message').textContent = "⛔ No number!";
+        //When player wins!
+    } else if(guess===secretNumber){
+        document.querySelector('.message').textContent="Correct Number!";
+
+        document.querySelector('body').style.backgroundColor='#60b347';
+
+        document.querySelector('.number').style.width='30rem';
+        document.querySelector('.number').textContent= secretNumber;
+
+        //when guess is too low
+    } else if (guess>secretNumber) {
+        if(score >1) {
+        document.querySelector('.message').textContent = " Too high!";
+        score--;
+        document.querySelector('.score').textContent =score;
+    } else {
+        document.querySelector('.message').textContent = " you lost the game!";
+        document.querySelector('.score').textContent =0;
+    }
+    }
+    //when guess is too low
+    else if (guess<secretNumber) {
+        if(score >1) {
+            document.querySelector('.message').textContent = " Too Low!";
+            score--;
+            document.querySelector('.score').textContent =score;
+        } else {
+            document.querySelector('.message').textContent = " you lost the game!";
+            document.querySelector('.score').textContent =0;
+        }
+    }
+} );
+
+
+//restarting the game
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 0;
+    
+    document.querySelector('message').textContent = "start guessing...";
+});
